3f05d2f34f563932b571bc26b45e0f43664c6e79
jack <wulei19922@gmail.com>
2025-02-24 03:12:53 +0800

    v1.1版本

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 git  pull
-cd jeecg-boot
+cd /data/code/JeecgBoot/jeecg-boot
 mvn clean install -DskipTest
-cd ../jeecgboot-vue3
+cd  /data/code/JeecgBoot/jeecg-boot/jeecg-module-system/jeecg-system-start
+docker build  -t  jeecg-boot-system:latest  .
+cd /data/code/JeecgBoot/jeecgboot-vue3
 pnpm run build
-docker-compose down
+docker build  -t  jeecgboot-vue3:latest  .
+cd /data/code/JeecgBoot
 docker-compose up -d
