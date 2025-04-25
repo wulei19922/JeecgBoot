1f38e19be5e30887886b59d9f5a81023cc78f840
jack <wulei19922@gmail.com>
2025-04-26 01:55:15 +0800

    第二版本机器人 kafka配置

--- jeecg-boot/jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/qe/config/KafkaProducerConfig.java
+++ jeecg-boot/jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/qe/config/KafkaProducerConfig.java
@@ -20,7 +20,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.226:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
